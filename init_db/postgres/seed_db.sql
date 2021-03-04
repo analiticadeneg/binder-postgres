8c39ec524ffd2e0d748f8d9442899dffa9a3d2fa
analiticadeneg <70983326+analiticadeneg@users.noreply.github.com>
2021-03-04 13:33:49 -0300

    Update seed_db.sql

--- init_db/postgres/seed_db.sql
+++ init_db/postgres/seed_db.sql
@@ -1,6 +1,12 @@
 -- Demo PostgreSQL Database initialisation
 
-DROP TABLE IF EXISTS quickdemo CASCADE;
-CREATE TABLE quickdemo(id INT, name VARCHAR(20), value INT);
-INSERT INTO quickdemo VALUES(1,'This',12);
-INSERT INTO quickdemo VALUES(2,'That',345);
+DROP TABLE IF EXISTS empleados CASCADE;
+
+CREATE TABLE empleados(id INT, nombre VARCHAR(20), edad INT);
+INSERT INTO empleados VALUES (1,'Roberto García', 32);
+INSERT INTO empleados VALUES (2,'Carlos Perez', 23);
+INSERT INTO empleados VALUES (3,'Verónica Vazquez', 28);
+INSERT INTO empleados VALUES (4,'Luciano López', 44);
+INSERT INTO empleados VALUES (5,'Sandra Segura', 48);
+INSERT INTO empleados VALUES (6,'Patricia Portela', 42);
+INSERT INTO empleados VALUES (7,'Pablo Pinto', 32);
